3735bbbfda973f1aee83dedcf4a7bca4403d2145
istoeckl <istoeckl@htlbasteyr.onmicrosoft.com>
2022-09-27 09:35:29 +0200

    SpielfeldView

--- src/main/java/com/example/itp_projekt_snake/View/SpielfeldView.java
+++ src/main/java/com/example/itp_projekt_snake/View/SpielfeldView.java
@@ -1,4 +1,78 @@
 package com.example.itp_projekt_snake.View;
 
-public class SpielfeldView {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Light;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javafx.application.Application.launch;
+
+public class SpielfeldView extends Application {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+    private static final String[] FOODS_IMAGE = new String[]{"/img/ic_orange.png", "/img/ic_apple.png", "/img/ic_cherry.png",
+            "/img/ic_berry.png", "/img/ic_coconut_.png", "/img/ic_peach.png", "/img/ic_watermelon.png", "/img/ic_orange.png",
+            "/img/ic_pomegranate.png"};
+
+    private static final int RIGHT = 0;
+    private static final int LEFT = 1;
+    private static final int UP = 2;
+    private static final int DOWN = 3;
+
+    private GraphicsContext gc;
+    private List<Light.Point> snakeBody = new ArrayList();
+    private Light.Point snakeHead;
+    private Image foodImage;
+    private int foodX;
+    private int foodY;
+    private boolean gameOver;
+    private int currentDirection;
+    private int score = 0;
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Snake-Spiel");
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        gc = canvas.getGraphicsContext2D();
+        drawBackground(gc);
+        drawScore();
+    }
+
+    private void drawBackground(GraphicsContext gc) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("bbdffb"));
+                } else {
+                    gc.setFill(Color.web("90cbf9"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+    }
+
+    private void drawScore() {
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Arial", 35));
+        gc.fillText("Score: " + score, 10, 30);
+    }
 }
+
