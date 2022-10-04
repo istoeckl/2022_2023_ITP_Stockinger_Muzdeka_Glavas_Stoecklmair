7e9d65529472821114cfd7a08dc3237453461ece
istoeckl <istoeckl@htlbasteyr.onmicrosoft.com>
2022-10-04 10:04:03 +0200

    SpielfeldView

--- src/main/java/com/example/itp_projekt_snake/Controller/SpielfeldController.java
+++ src/main/java/com/example/itp_projekt_snake/Controller/SpielfeldController.java
@@ -1,4 +1,49 @@
 package com.example.itp_projekt_snake.Controller;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
 public class SpielfeldController {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+
+    private int score = 0;
+    public void drawBackground(GraphicsContext gc) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                String[][] feld = new String[i][j];
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("bbdffb"));
+                } else {
+                    gc.setFill(Color.web("90cbf9"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+    }
+
+    public void drawStartSnake(GraphicsContext gc) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                //Startpunkt Schlange
+                if (i==4&&j==8){
+                    System.out.println("slödgj");
+                    gc.setFill(Color.web("#000000"));
+                }
+            }
+        }
+        gc.fillRect(4 * SQUARE_SIZE, 8 * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    }
+
+
+    public void drawScore(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Arial", 35));
+        gc.fillText("Score: " + score, 10, 30);
+    }
+
 }
--- src/main/java/com/example/itp_projekt_snake/View/SpielfeldView.java
+++ src/main/java/com/example/itp_projekt_snake/View/SpielfeldView.java
@@ -1,5 +1,6 @@
 package com.example.itp_projekt_snake.View;
 
+import com.example.itp_projekt_snake.Controller.SpielfeldController;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,7 +18,6 @@ import java.util.List;
 import static javafx.application.Application.launch;
 
 public class SpielfeldView extends Application {
-
     private static final int WIDTH = 800;
     private static final int HEIGHT = WIDTH;
     private static final int ROWS = 20;
@@ -28,7 +28,7 @@ public class SpielfeldView extends Application {
     private int score = 0;
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("Snake-Spiel");
         Group root = new Group();
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
@@ -37,28 +37,13 @@ public class SpielfeldView extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         gc = canvas.getGraphicsContext2D();
-        drawBackground(gc);
-        drawScore();
+        SpielfeldController control=new SpielfeldController();
+        control.drawBackground(gc);
+        control.drawScore(gc);
+        control.drawStartSnake(gc);
     }
 
-    private void drawBackground(GraphicsContext gc) {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMNS; j++) {
-                String [][] feld = new String[i][j];
-                if ((i + j) % 2 == 0) {
-                    gc.setFill(Color.web("bbdffb"));
-                } else {
-                    gc.setFill(Color.web("90cbf9"));
-                }
-                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-            }
-        }
-    }
 
-    private void drawScore() {
-        gc.setFill(Color.BLACK);
-        gc.setFont(new Font("Arial", 35));
-        gc.fillText("Score: " + score, 10, 30);
+
     }
-}
 
