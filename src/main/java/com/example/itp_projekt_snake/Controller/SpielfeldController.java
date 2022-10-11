794fc58b0ede61491fc5fa574bc426afb7874d54
istoeckl <istoeckl@htlbasteyr.onmicrosoft.com>
2022-10-11 11:17:37 +0200

    verb

--- src/main/java/com/example/itp_projekt_snake/Controller/SpielfeldController.java
+++ src/main/java/com/example/itp_projekt_snake/Controller/SpielfeldController.java
@@ -7,33 +7,13 @@ import javafx.scene.text.Font;
 
 import java.util.Random;
 
+import static com.example.itp_projekt_snake.Model.Spielfeld.SQUARE_SIZE;
+
 
 public class SpielfeldController {
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = WIDTH;
-
-    private static final int ROWS = 20;
-    private static final int COLUMN = 20;
-    private static final int SQUARE_SIZE = WIDTH / ROWS;
-
     private String pink = "ffc0cb";
     private String hell = "90cbf9";
     private String dunkel = "bbdffb";
-    private int score = 0;
-
-    public void drawBackground(GraphicsContext gc) {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMN; j++) {
-                String[][] feld = new String[i][j];
-                if ((i + j) % 2 == 0) {
-                    gc.setFill(Color.web(dunkel));
-                } else {
-                    gc.setFill(Color.web(hell));
-                }
-                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-            }
-        }
-    }
 
 
     //lea glavas
@@ -48,15 +28,6 @@ public class SpielfeldController {
 
 
         gc.fillRect(value1 * SQUARE_SIZE, value2 * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-    }
-
-
-
-
-    public void drawScore(GraphicsContext gc) {
-        gc.setFill(Color.BLACK);
-        gc.setFont(new Font("Arial", 35));
-        gc.fillText("Score: " + score, 10, 30);
     }
 
 }
